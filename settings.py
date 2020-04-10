c0980e3a52fb33b4e6b90a3f240a35a9dcd4f5c6
Matej Šrubař <matosrubar@gmail.com>
2020-04-10 14:43:42 +0200

    Minor performance enhanced

--- settings.py
+++ settings.py
@@ -16,6 +16,9 @@ with open('conf.json') as f:
 
 
 app = Flask(__name__)
+
+# Disables unused SQLAlchemy tracking for performance reasons
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
 db = SQLAlchemy(app)
